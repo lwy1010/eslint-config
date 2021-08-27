70ce82832cf73375b359fca3bd7bfbbfa281a202
lwy1010 <im.leewy@gmail.com>
2021-08-27 09:51:20 +0800

    feat: lint typescript is supported

--- packages/base/index.js
+++ packages/base/index.js
@@ -6,4 +6,19 @@ module.exports = {
     // using the const declaration is better.
     "prefer-const": "error",
   },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+      },
+      rules: {
+        // disallow unused vars.
+        '@typescript-eslint/no-unused-vars': 'error'
+      },
+    },
+  ],
 };
